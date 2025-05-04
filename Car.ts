bbb63dc47583470ef03b32f19d951a141299e092
ruscoe <danruscoe@protonmail.com>
2025-05-03 18:51:51 -0700

    Rotate cars when they veer off the road.

--- Car.ts
+++ Car.ts
@@ -8,6 +8,7 @@ export class Car {
     isVeering: boolean = false;
     // -1 veer left, 1 veer right
     veerDirection: number = 0;
+    rotation: number = 0;
 
     constructor(x: number, y: number, image: HTMLImageElement) {
         this.x = x;
@@ -20,11 +21,31 @@ export class Car {
 
         if (this.isVeering) {
             this.x += this.veerDirection * 5;
+
+            const rotationSpeed = 0.05;
+            // Apply rotation based on veering direction (~30 degrees as radians)
+            const maxRotation = 0.5;
+
+            if (this.veerDirection === -1 && this.rotation > -maxRotation) {
+                this.rotation -= rotationSpeed;
+            } else if (this.veerDirection === 1 && this.rotation < maxRotation) {
+                this.rotation += rotationSpeed;
+            }
         }
     }
 
     draw(ctx: CanvasRenderingContext2D) {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.save();
+
+        // Move to center of car.
+        ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
+
+        // Apply rotation.
+        ctx.rotate(this.rotation);
+
+        ctx.drawImage(this.image, -this.width / 2, -this.height / 2, this.width, this.height);
+
+        ctx.restore();
     }
 
     isOffScreen(): boolean {
